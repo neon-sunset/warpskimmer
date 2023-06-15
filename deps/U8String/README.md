73f17902d6da19d258a2e7f2acab543b88b3a43d
neon-sunset <neon_sunset@outlook.com>
2023-06-15 22:39:20 +0300

    turns out git subtrees are even worse than submodules, what the fuck

--- deps/U8String/README.md
+++
@@ -1,2 +0,0 @@
-# U8String
-[work-in-progress] Highly functional and performant UTF-8 string primitive for C#
--- deps/U8String/src/U8String.Enumeration.cs
+++
@@ -1,9 +0,0 @@
-using System.Runtime.CompilerServices;
-
-namespace U8Primitives;
-
-public readonly partial struct U8String
-{
-    [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    public ReadOnlySpan<byte>.Enumerator GetEnumerator() => AsSpan().GetEnumerator();
-}
