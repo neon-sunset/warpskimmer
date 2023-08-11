8f28b49e6519f6cf6fb38330ca0e1c9610944d7e
neon-sunset <neon_sunset@outlook.com>
2023-08-11 14:35:18 +0300

    fix: track U8String changes

--- src/Tag.cs
+++ src/Tag.cs
@@ -41,7 +41,7 @@ public readonly record struct Tag
         {
             var tagValue = U8Marshal.Slice(allTags, tagRanges.IndexUnsafe(i));
             var splitOffset = IndexOfSeparator(
-                ref MemoryMarshal.GetReference(U8Marshal.GetSpan(tagValue)));
+                ref MemoryMarshal.GetReference(U8Marshal.AsSpan(tagValue)));
 
             tagSpan.IndexUnsafe(i) = splitOffset < 16
                 ? new(U8Marshal.CreateSplitPair(tagValue, splitOffset, 1))
